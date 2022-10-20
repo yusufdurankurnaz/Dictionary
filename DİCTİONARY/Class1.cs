d4661b570960e2eb198614c4a784bfd11ccaba88
yusufdurankurnaz <113217782+yusufdurankurnaz@users.noreply.github.com>
2022-10-21 01:57:37 +0300

    Add project files.

---
+++ DİCTİONARY/Class1.cs
@@ -0,0 +1,25 @@
+﻿using dictionary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        void Main(string[] args)
+        {
+            mylist<int, string> ogrenciler = new mylist<int, string>();
+            ogrenciler.add(1, "yusuf");
+            ogrenciler.add(2, "yusuf");
+            ogrenciler.add(3, "yusuf");
+            ogrenciler.add(4, "yusuf");
+            ogrenciler.add(5, "yusuf");
+
+
+        }
+
+    }
+}
---
+++ DİCTİONARY/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+
+
+    class mylist<Tkey, Tvalue>
+    {
+
+        Tkey[] array;
+        Tvalue[] Aarray;
+        Tkey[] temparray;
+        Tvalue[] tempAarray;
+
+        public mylist()
+        {
+            array = new Tkey[0];
+            Aarray = new Tvalue[0];
+        }
+
+        public void add(Tkey key, Tvalue value)
+        {
+            //int sayi = 0;
+            temparray = array;
+            tempAarray = Aarray;
+            array = new Tkey[array.Length + 1];
+            Aarray = new Tvalue[Aarray.Length + 1];
+            for (int i = 0; i < temparray.Length; i++)
+            {
+                temparray[i] = array[i];
+            }
+
+            for (int i = 0; i < tempAarray.Length; i++)
+            {
+                tempAarray[i] = Aarray[i];
+            }
+
+            array[array.Length - 1] = key;
+            Aarray[Aarray.Length - 1] = value;
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
